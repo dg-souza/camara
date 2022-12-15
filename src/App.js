ec58473b1a76c2cba364c2b338fd5e15af541058
davisg <davi.gama@triadsystems.com.br>
2022-12-15 13:37:39 -0300

    Criado a função de filtro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { api } from './services/api'
 
@@ -13,21 +13,49 @@ import {
 import ListCard from './components/ListCard'
 
 const App = () => {
+  const [deputados, setDeputados] = useState([])
+  const [deputadosFiltrados, setDeputadosFiltrados] = useState('')
+  const [searchInput, setSearchInput] = useState('')
+
   useEffect(() => {
+    getAllDeputados()
   }, [])
+
+  function getAllDeputados() {
+    api.get('/deputados?itens=50')
+      .then(res => {
+        setDeputados(res.data.dados)
+        console.log(deputados)
+      })
+  }
+
+  function filterByName(value) {
+    setSearchInput(value)
+    if(value !== '') {
+      const filteredResults = deputados.filter((item) => {
+        return Object.values(item).join('').toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      })
+      setDeputadosFiltrados(filteredResults)
+    } else {
+      setDeputadosFiltrados(deputados)
+    }
+  }
 
   return(
     <Content>
       <InputForm>
-        <Input />
-
-        <select>
-          <option>Teste</option>
-          <option>Teste2</option>
-        </select>
+        <Input placeholder='Pesquise por deputado ou sigla do partido' onChange={(e) => filterByName(e.target.value)} />
       </InputForm>
 
-      <ListCard />
+      {
+        searchInput.length > 0 ?
+        <>
+          <ListCard deputados={deputadosFiltrados} />
+        </> :
+        <>
+          <ListCard deputados={deputados} />
+        </>
+      }
     </Content>
   )
 }
